080b8ba2ec4a31d548248069a3d53955e3e45b1e
勇介　齊藤 <wisteriaq9@gmail.com>
2024-02-23 21:07:08 +0900

    名前をメインに変更

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "DxLib.h"
+#include "Picture.h"
+#include "Title.h"
+// プログラムは WinMain から始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
+	{
+		return -1;			// エラーが起きたら直ちに終了
+	}
+	Picture picture{};
+	picture.Load();//全画像の読み込み
+
+	TITLE title;
+	title.Set(picture);
+
+	WaitKey();				// キー入力待ち
+
+	DxLib_End();				// ＤＸライブラリ使用の終了処理
+
+	return 0;				// ソフトの終了 
+}
