a68914d0f56a4bf591bf6c09418559852051fed2
勇介　齊藤 <wisteriaq9@gmail.com>
2024-03-13 19:41:26 +0900

    複数行読み込む

--- Score.cpp
+++ Score.cpp
@@ -19,18 +19,20 @@ void Score::Load(){//Score
 		std::stringstream bpm(str);
 		bpm >> BPM;
 		//1行だけ読み込んでタイミング設定
-		複数行よみたい
-		while (std::getline(ifs, str, ',')) {
-			std::stringstream ss(str);
-			if (str != "") {
-				notes.push_back(note);
-				notes[Notes_Number].SetTiming(timing);
-				Notes_Number++;
+		while (!ifs.eof()) {
+			while (std::getline(ifs, str, ',')) {
+				std::stringstream ss(str);
+				if (str != "") {
+					notes.push_back(note);
+					notes[Notes_Number].SetTiming(timing);
+					Notes_Number++;
+				}
+				//リズムの定義をしっかりしたい
+				timing += 60 / BPM;
 			}
-			リズムの定義をしっかりしたい
-			timing+=60/BPM;
-			
 		}
+
+		
 	}
 	Notes_Max = Notes_Number;
 	Notes_Number = 0;
@@ -39,14 +41,14 @@ void Score::Load(){//Score
 void Score::Out(Picture picture,GAMETIME Time) {
 	for (j = 0; j < Notes_Number; j++) {
 		notes[j].Move(picture, Time);
-		DrawFormatString(50, 100+j*15, GetColor(0, 0, 0), "%f", notes[j].GetTiming());
+		DrawFormatString(50, 100+j*15, GetColor(0, 0, 0), "%d", Notes_Number);
 	}
 
 	DrawFormatString(25, 100, GetColor(0, 0, 0), "%f", BPM);
 
 	
 	//ノーツの間隔をあける条件式
-	if (Notes_Number < Notes_Max) {
+	if (Notes_Number < Notes_Max-1) {
 		if (notes[Notes_Number].GetTiming()< Time.GetElapsedTime()) {
 			Notes_Number++;
 		}
